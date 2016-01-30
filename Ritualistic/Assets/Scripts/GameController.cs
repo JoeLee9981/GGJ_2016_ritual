b5c45c1ea5508c8d3b6d413b48b776dd52e14c36
JoeLee9981 <khazidea99@hotmail.com>
2016-01-30 04:22:41 -0700

    Added pause

--- Ritualistic/Assets/Scripts/GameController.cs
+++ Ritualistic/Assets/Scripts/GameController.cs
@@ -10,6 +10,15 @@ public class GameController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	   
-	}
+        if (Input.GetButtonDown("Cancel")) {
+            if(Time.timeScale == 0) {
+                Time.timeScale = 1;
+            }
+            else {
+                Time.timeScale = 0;
+            }
+            GameManager.GetInstance().Active = !GameManager.GetInstance().Active;
+        }
+
+    }
 }
