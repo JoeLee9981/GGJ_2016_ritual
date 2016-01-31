450ea982734559f05d7a2160b09b6c0d55baacf3
JoeLee9981 <khazidea99@hotmail.com>
2016-01-31 14:57:02 -0700

    Stuff

--- Ritualistic/Assets/Scripts/MonsterController.cs
+++ Ritualistic/Assets/Scripts/MonsterController.cs
@@ -46,43 +46,23 @@ public class MonsterController : MonoBehaviour {
                     Vector3 transformVec = GetDirectionVector(transform.position, GameManager.GetInstance().ActivePlayer.transform.position) * enemyCharacter.MovementSpeed * Time.deltaTime;
                     transformVec.y = 0;
                     transform.position += transformVec;
+                    transform.LookAt(playerController.GetComponent<Transform>());
                 }
                 else {
                     playerCollisionDetected = false;
                 }
             }
+
         }
 
         PerformCollisionDetection();
     }
 
-    void FixedUpdate() {
-
-        if (GameManager.GetInstance().Active) {
-            // Generate a plane that intersects the transform's position with an upwards normal.
-            Plane playerPlane = new Plane(Vector3.up, transform.position);
-
-            // Generate a ray from the cursor position
-            Ray ray = Camera.main.ScreenPointToRay(playerController.GetComponent<Transform>().position);
-
-            // Determine the point where the cursor ray intersects the plane.
-            // This will be the point that the object must look towards to be looking at the mouse.
-            // Raycasting to a Plane object only gives us a distance, so we'll have to take the distance,
-            //   then find the point along that ray that meets that distance.  This will be the point
-            //   to look at.
-            float hitdist = 0.0f;
-            // If the ray is parallel to the plane, Raycast will return false.
-            if (playerPlane.Raycast(ray, out hitdist)) {
-                // Get the point along the ray that hits the calculated distance.
-                Vector3 targetPoint = ray.GetPoint(hitdist);
-
-                // Determine the target rotation.  This is the rotation if the transform looks at the target point.
-                Quaternion targetRotation = Quaternion.LookRotation(targetPoint - transform.position);
-
-                // Smoothly rotate towards the target point.
-                transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
-            }
-        }
+    void FixedUpdate() { 
+        if (GameManager.GetInstance().Active) 
+            {
+                
+            }       
     }
 
     void OnCollisionEnter(Collision col) {
@@ -106,6 +86,7 @@ public class MonsterController : MonoBehaviour {
             if (hit.collider.gameObject.CompareTag("Walls")) {
                 collisionDetected = true;
             }
+          
         }
         /*
         // Collision Detection Rays - Front
