4713af85426b07afa2decc3c288ebc2933608b59
mlindhorst <mlindhorst.24587@gmail.com>
2016-01-31 14:58:28 -0700

    Keys disappear from chests.

--- Ritualistic/Assets/Scripts/TreasureChestController.cs
+++ Ritualistic/Assets/Scripts/TreasureChestController.cs
@@ -16,6 +16,24 @@ public class TreasureChestController : MonoBehaviour {
             PlayerController player = playerObject.GetComponent<PlayerController>();
             player.playerCharacter.AddToInventory(new KeyItem(keyType.ToString(), keyType));
             isOpen = true;
+            StartCoroutine(TakeKey());
         }
+    }
+    
+    private IEnumerator TakeKey()
+    {
+        yield return new WaitForSeconds(1.5f);
+        Transform keyObject = null;
+        GameObject prefabObject = transform.parent.gameObject;
+        if (keyType.ToString() == "LOW_KEY")
+        {
+            keyObject = prefabObject.GetComponentInChildren<Transform>().Find("basickey");
+            keyObject.FindChild("pPipe6").GetComponent<MeshRenderer>().enabled = false;
+        }
+        else
+        {
+            prefabObject.GetComponentInChildren<Transform>().Find("specialkey");
+            keyObject.FindChild("pCylinder6").GetComponent<MeshRenderer>().enabled = false;
+        }        
     }
 }
